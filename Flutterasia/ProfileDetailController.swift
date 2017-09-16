7407226e27845554902be1fba091808699060b31
Imron Rosdiana <imron02@gmail.com>
2017-09-16 18:41:29 +0700

    Add profile info page

---
+++ Flutterasia/ProfileDetailController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileDetailController.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 9/15/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import UIKit
+
+class ProfileDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
