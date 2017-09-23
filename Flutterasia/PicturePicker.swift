2133292ce42e38b1d7f0ab0993679a7fad9332e8
Imron Rosdiana <imron02@gmail.com>
2017-09-23 10:52:00 +0700

    Add picture picker swift file

---
+++ Flutterasia/PicturePicker.swift
@@ -0,0 +1,40 @@
+//
+//  PicturePicker.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 9/19/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import UIKit
+
+extension RegisterController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @objc func openGallery() -> Void {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            changeImage = true
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
