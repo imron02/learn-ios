4dcda4aa3bbc459262aaf7cd4dbeaf4d11bb607c
Imron Rosdiana <imron02@gmail.com>
2017-10-07 17:26:31 +0700

    Add login view model

---
+++ Flutterasia/LoginViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewModel.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 07/10/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewModel {
+    typealias CompletionHandler = (Bool, String?) -> Void
+    private var user = User()
+    
+    var fullName: String {
+        return user.fullName!
+    }
+    
+    var email: String {
+        return user.email!
+    }
+    
+    var password: String {
+        return user.password!
+    }
+    
+    var phone: String {
+        return user.phone!
+    }
+    
+    func login(email: String, password: String, completion: @escaping CompletionHandler) -> Void {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let firebaseError = error {
+                completion(true, firebaseError.localizedDescription)
+                return
+            }
+
+            completion(true, nil)
+        }
+    }
+}
