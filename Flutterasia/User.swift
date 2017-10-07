47b88353139b44422d31fc962e0b942241c43083
Imron Rosdiana <imron02@gmail.com>
2017-10-07 17:26:40 +0700

    Add user model

---
+++ Flutterasia/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 07/10/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var fullName: String?
+    var email: String?
+    var password: String?
+    var phone: String?
+    var profileImageUrl: String?
+}
