2b2ee18547559cbb40d0a7609f69250e38179640
Imron Rosdiana <imron.rosdiana@dataon.com>
2017-12-12 10:49:18 +0700

    Add more page

---
+++ Flutterasia/MoreMenuTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoreMenuTableViewController.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 10/12/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import UIKit
+
+class MoreMenuTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
