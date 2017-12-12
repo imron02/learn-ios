9e7c65f5324b2ca596f3d4ccc9a855a570f105a1
Imron Rosdiana <imron.rosdiana@dataon.com>
2017-12-12 10:53:16 +0700

    Add storyboard & swift for update profile

---
+++ Flutterasia/FormProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FormProfileViewController.swift
+//  Flutterasia
+//
+//  Created by Imron Rosdiana on 10/12/17.
+//  Copyright © 2017 Imron Rosdiana. All rights reserved.
+//
+
+import UIKit
+
+class FormProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
